2571495f0e663641ef69a1b1fe1e10267cdc8d47
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-04-29 22:18:11 -0400

    Added quotes variable

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+
+const quote = [
+  "Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa",
+  "When you reach the end of your rope, tie a knot in it and hang on. -Franklin D. Roosevelt",
+  "Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead",
+  "Don't judge each day by the harvest you reap but by the seeds that you plant. -Robert Louis Stevenson",
+  "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+];
 
 
 
